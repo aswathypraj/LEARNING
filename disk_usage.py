c2161890153df8133d0d209d37a428db2461301b
Aswathy Raj <aswathypraj@gmail.com>
2020-09-16 19:00:01 +0400

    Added space to print statement texts

--- disk_usage.py
+++ disk_usage.py
@@ -22,7 +22,7 @@ from psutil._common import bytes2human
 
 def main():
     templ = "%-17s %8s %8s %8s %5s%% %9s  %s"
-    print(templ % ("Device", "Total", "Used", "Free", "Use ", "Type",
+    print(templ % ("Device ", "Total ", "Used", "Free", "Use ", "Type",
                    "Mount"))
     for part in psutil.disk_partitions(all=False):
         if os.name == 'nt':
